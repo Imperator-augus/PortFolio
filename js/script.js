809341fc61dda695703eefb6caa0e73e10326a65
YE CHAN <qjdrptmzk@naver.com>
2022-07-30 00:42:55 +0900

    경량화작업

--- js/script.js
+++ js/script.js
@@ -15,27 +15,6 @@ $(function() {
   });
 });
 
-
-//OWL CAROSEL TESTIMONIAL
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    dots:true,
-    dotsEach:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
-})
-
 /*Preloader*/
 //<![CDATA[
 $(window).load(function() { // makes sure the whole site is loaded
@@ -45,4 +24,37 @@ $(window).load(function() { // makes sure the whole site is loaded
 })
 //]]>
 
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  spaceBetween: 30,// 슬라이드 사이 여백
+  slidesPerView : 'auto', // 한 슬라이드에 보여줄 갯수        
+  centeredSlides: true,//센터모드        
+  autoplay: {//자동슬라이드 (false-비활성화)          
+    delay: 3500, // 시간 설정          
+    disableOnInteraction: false, // false-스와이프 후 자동 재생        
+  },
+  loop : false,// 슬라이드 반복 여부
+  loopAdditionalSlides : 1, // 슬라이드 반복 시 마지막 슬라이드에서 다음 슬라이드가 보여지지 않는 현상 수정         
+  pagination: { // 호출(pager) 여부          
+    el: ".swiper-pagination", //버튼을 담을 태그 설정          
+    clickable: true, // 버튼 클릭 여부        
+  },
+  breakpoints: {
+    '@0.75': {
+      spaceBetween: 20,
+    },
+    '@1.00': {
+      spaceBetween: 40,
+    },
+    '@1.50': {
+      spaceBetween: 50,
+    }
+  }
+});
 
