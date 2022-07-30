e9faa12f908eec499631fd4e0bfbc118ab9d54b6
YE CHAN <qjdrptmzk@naver.com>
2022-07-30 15:37:41 +0900

    오토슬라이드주석

--- js/script.js
+++ js/script.js
@@ -27,10 +27,10 @@ const swiper = new Swiper('.swiper', {
   spaceBetween: 30,// 슬라이드 사이 여백
   slidesPerView : 'auto', // 한 슬라이드에 보여줄 갯수        
   centeredSlides: true,//센터모드        
-  autoplay: {//자동슬라이드 (false-비활성화)          
-    delay: 3500, // 시간 설정          
-    disableOnInteraction: false, // false-스와이프 후 자동 재생        
-  },
+  // autoplay: {//자동슬라이드 (false-비활성화)          
+  //   delay: 6000, // 시간 설정          
+  //   disableOnInteraction: false, // false-스와이프 후 자동 재생        
+  // },
   loop : false,// 슬라이드 반복 여부
   loopAdditionalSlides : 1, // 슬라이드 반복 시 마지막 슬라이드에서 다음 슬라이드가 보여지지 않는 현상 수정         
   pagination: { // 호출(pager) 여부          
